ad331f1229558620512d3cfeac660d7917efb8e3
Reaishma N <hayagreevar9018@gmail.com>
2025-07-14 14:25:15 +0530

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,1 +1,25 @@
+import React from "react";
+import Navigation from "@/components/Navigation";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Features from "@/components/Features";
+import Calculator from "@/components/Calculator";
+import Connect from "@/components/Connect";
+import Footer from "@/components/Footer";
+
+const Home: React.FC = () => {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <Hero />
+      <About />
+      <Features />
+      <Calculator />
+      <Connect />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
 
